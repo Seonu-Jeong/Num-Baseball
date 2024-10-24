fc91959d3e67a4cef0c31b1ded146da2216f3272
Jeong Seon U <sw.jeong97@gmail.com>
2024-10-25 04:03:45 +0900

    feat : BaseballGame 클래스 / 의존성 필드 추가

---
+++ src/main/java/org/example/numplay/util/BaseballGame.java
@@ -0,0 +1,10 @@
+package org.example.numplay.util;
+
+public class BaseballGame {
+
+    ConsoleInteraction consoleInteraction = new ConsoleInteraction();
+    NumGenerator numGenerator = new NumGenerator();
+
+
+
+}
