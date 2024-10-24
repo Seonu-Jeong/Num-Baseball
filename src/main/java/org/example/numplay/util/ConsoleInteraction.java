aff081302213df2c531e545869f9346995dd988d
Jeong Seon U <sw.jeong97@gmail.com>
2024-10-25 03:50:28 +0900

    feat : ConsoleInteraction 클래스 / 숫자입력 받기 기능 추가

--- src/main/java/org/example/numplay/util/ConsoleInteraction.java
+++ src/main/java/org/example/numplay/util/ConsoleInteraction.java
@@ -29,4 +29,54 @@ public class ConsoleInteraction {
 
         return menu;
     }
+
+    public int getNum(int level){
+
+        int result;
+
+        while(true) {
+            System.out.println("숫자를 입력하세요");
+
+            Scanner sc = new Scanner(System.in);
+
+            try {
+                String num_string = sc.nextLine();
+
+                int num = Integer.parseInt(num_string);
+
+                int[] digit = new int[10];
+
+                for (int i = 0; i < num_string.length(); i++) {
+                    digit[num_string.charAt(i) - '0']++;
+                }
+
+                if (digit[0] >= 1)
+                    throw new Exception("0을 포함할 수 없습니다");
+
+                int cnt = 0;
+
+                for (int val : digit) {
+                    if (val > 1)
+                        throw new Exception("중복된 숫자를 사용할 수 없습니다");
+
+                    cnt += val;
+                }
+
+                if (cnt != level)
+                    throw new Exception(level+"자리 숫자를 입력해주세요");
+
+                result = num;
+            } catch (NumberFormatException e) {
+                System.out.println("숫자만 입력해주세요");
+                continue;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+            break;
+        }
+
+        return result;
+    }
 }
